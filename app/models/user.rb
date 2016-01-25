33121262b21d0e7c2294544bb1b18ace6aa8919b
zalbo <sbabbari@gmail.com>
2016-01-25 11:39:31 +0100

    add nickname to user and login with nickname and mail in the same field

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,21 @@ class User < ActiveRecord::Base
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   ##
 
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(nickname) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      if conditions[:nickname].nil?
+        where(conditions).first
+      else
+        where(nickname: conditions[:nickname]).first
+      end
+    end
+  end
+
+  attr_accessor :login
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,7 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
@@ -20,6 +21,8 @@ Devise.setup do |config|
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
   # available as additional gems.
   require 'devise/orm/active_record'
+
+
 
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
@@ -256,4 +259,9 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+  config.authentication_keys = [ :login ]
+  config.reset_password_keys = [ :nickname ]
+  config.confirmation_keys = [ :nickname ]
+
+
 end
