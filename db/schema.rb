663d9d7ec6f6f7a61200d43228ef83eb746c288f
zalbo <sbabbari@gmail.com>
2016-01-25 14:58:11 +0100

    add rake admin setup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160122083121) do
+ActiveRecord::Schema.define(version: 20160125134327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20160122083121) do
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.string   "nickname"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
