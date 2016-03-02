bc44ea9851d9453d08af2ffffe1fbb775ebc912b
zalbo <sbabbari@gmail.com>
2016-03-02 22:36:51 +0100

    in page index can you edit and delete the page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -35,6 +35,7 @@ class PagesController < ApplicationController
   def create
     @project = Project.find(params[:page][:project_id])
     @page = Page.new(page_params)
+    binding.pry
 
     if @page.save
 
@@ -47,25 +48,23 @@ class PagesController < ApplicationController
   # PATCH/PUT /pages/1
   # PATCH/PUT /pages/1.json
   def update
-    respond_to do |format|
-      if @page.update(page_params)
-        format.html { redirect_to @page, notice: 'Page was successfully updated.' }
-        format.json { render :show, status: :ok, location: @page }
-      else
-        format.html { render :edit }
-        format.json { render json: @page.errors, status: :unprocessable_entity }
-      end
+    @page.update(title: params[:page][:title])
+
+    if @page.update(title: params[:page][:title])
+      redirect_to "/projects/#{@page.project_id}/pages"
+    else
+      redirect_to "/projects/#{@page.project_id}/pages/#{@page.id}/edit"
     end
   end
 
   # DELETE /pages/1
   # DELETE /pages/1.json
   def destroy
+    binding.pry
     @page.destroy
-    respond_to do |format|
-      format.html { redirect_to pages_url, notice: 'Page was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+
+    redirect_to "/projects/#{@page.project_id}/pages"
+
   end
 
   private
