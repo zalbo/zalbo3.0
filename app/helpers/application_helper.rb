0251bd694f3451ac861dfd522805a04998713eba
zalbo <sbabbari@gmail.com>
2016-04-07 19:49:15 +0200

    add array for contents

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def render_devise_message(msg)
     @msg_dvs_rnded = msg.slice((msg.index('<h2>'))..(msg.index(':</h2>'))).sub!("<h2>", "")
   end
+
+  def find_contents(project_id)
+    @contents = []
+    Content.all.each do |content|
+      if content.project_id == project_id
+
+          @contents << content
+        end
+    end
+  end
 end
